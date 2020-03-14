51d28fc5b158013b8dc37840f53d1de904810f83
张战锋 <zhangzhanfeng@zhangzhanfengdeMacBook-Pro.local>
2020-03-14 14:37:57 +0800

    feat:添加eslint

--- src/views/login/Index.vue
+++ src/views/login/Index.vue
@@ -2,30 +2,24 @@
   <div class="login">
     <div class="login-box">
       <div class="login-title">后台管理系统</div>
-      <el-form :model="ruleForm"
-               :rules="rules"
-               ref="ruleForm"
-               label-width="0px"
-               class="login-content">
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="login-content">
         <el-form-item prop="userName">
-          <el-input v-model="ruleForm.userName"
-                    placeholder="用户名">
-            <el-button slot="prepend"
-                       icon="el-icon-user-solid"></el-button>
+          <el-input v-model="ruleForm.userName" placeholder="用户名">
+            <el-button slot="prepend" icon="el-icon-user-solid"></el-button>
           </el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input type="password"
-                    placeholder="密码"
-                    v-model="ruleForm.password"
-                    @keyup.enter.native="submitForm('ruleForm')">
-            <el-button slot="prepend"
-                       icon="el-icon-lock"></el-button>
+          <el-input
+            type="password"
+            placeholder="密码"
+            v-model="ruleForm.password"
+            @keyup.enter.native="submitForm('ruleForm')"
+          >
+            <el-button slot="prepend" icon="el-icon-lock"></el-button>
           </el-input>
         </el-form-item>
         <div class="login-btn">
-          <el-button type="primary"
-                     @click="submitForm('ruleForm')">登录</el-button>
+          <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
         </div>
         <p class="login-tips">Tips : 用户名和密码随便填。</p>
       </el-form>
@@ -34,7 +28,7 @@
 </template>
 
 <script>
-import { setStorage, getStorage } from '@utils/index';
+import { setStorage, getStorage } from '@utils/index'
 export default {
   data() {
     return {
@@ -43,27 +37,23 @@ export default {
         password: '123123'
       },
       rules: {
-        userName: [
-          { required: true, message: '请输入用户名', trigger: 'blur' }
-        ],
-        password: [
-          { required: true, message: '请输入密码', trigger: 'blur' }
-        ]
+        userName: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
       }
     }
   },
   methods: {
     submitForm(formName) {
-      this.$refs[formName].validate((valid) => {
+      this.$refs[formName].validate(valid => {
         if (valid) {
           // 请求登录接口后，设置token
-          setStorage('1111111');
-          this.$router.push('/');
+          setStorage('1111111')
+          this.$router.push('/')
         } else {
-          this.$message.warning('账号或密码格式不正确');
-          return false;
+          this.$message.warning('账号或密码格式不正确')
+          return false
         }
-      });
+      })
     }
   },
   mounted() {
@@ -79,8 +69,7 @@ export default {
 .login {
   position: relative;
   height: 100vh;
-  background: url(../../assets/images/error_cloud.png) repeat-x center bottom
-    $--color-bg-blue;
+  background: url(../../assets/images/error_cloud.png) repeat-x center bottom $--color-bg-blue;
   &-box {
     position: absolute;
     left: 50%;
